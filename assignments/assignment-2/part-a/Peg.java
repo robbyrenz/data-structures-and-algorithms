e0cab1bd806cb8973afbdf378b840918127602d9
Robby Renz <nxgrenz@gmail.com>
2018-10-11 19:45:59 -0500

    Peg.java is now actually 60% done, prev commit message was mistake

--- assignments/assignment-2/part-a/Peg.java
+++ assignments/assignment-2/part-a/Peg.java
@@ -3,3 +3,27 @@
  * Student Number: 3073208
  * Part A of Assignment 2
  */
+
+public class Peg {
+    
+    // fields
+    private String color;
+    private boolean empty;
+
+    // constructor
+    public Peg(String color, boolean empty) {
+        this.color = color;
+        this.empty = empty;
+    }
+
+    // getters
+    public String getColor() {
+        return color;
+    }
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    // setters
+    
+}
