b62629e6a458872d1556677e565cdfe46affe13b
Robby Renz <nxgrenz@gmail.com>
2018-11-09 11:39:02 -0600

    Add Lab 6 question pdf & started coding Team class for q1

---
+++ labs/lab6/Team.java
@@ -0,0 +1,20 @@
+/**
+ * @author Robby Renz Abeysinghe
+ * Student Number: 3073208
+ * Answer 1 of Lab 6
+ */
+
+public class Team {
+	// private fields
+	private String city;
+	private String name;
+	private int wins;
+	private int losses;
+	private int otl; // number of overtime/shootout wins
+	private int row // number of regulation overtime wins
+
+	// constructors
+	public Team() {
+
+	}
+
