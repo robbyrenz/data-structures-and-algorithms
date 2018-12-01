5380b257ae67b1e25d8806426f4e6a19244b054b
Robby Renz <nxgrenz@gmail.com>
2018-11-30 21:48:52 -0600

    q1 of assignment 4 is 90% done

--- assignments/assignment-4/Driver.java
+++ assignments/assignment-4/Driver.java
@@ -10,16 +10,28 @@ import java.io.FileNotFoundException;
 
 public class Driver {
 	public static void main(String[] args) throws FileNotFoundException {
-		Map<String, Integer> map = new ProbeHashMap<>();
+		Map<String,Integer> map = new ProbeHashMap<>();
 		Scanner keyboard = new Scanner(new File("PartA.txt")).useDelimiter("[^a-zA-Z']+");
 
 		while (keyboard.hasNext()) {
-			String word = keyboard.next();
+			String word = keyboard.next().toLowerCase();
 			if (map.get(word) == null)
 				map.put(word, 1);
 			else
 				map.put(word, map.get(word) + 1);
 		}
+
+		Entry<String,Integer> arr[] = new Entry[map.size()];
+
+		int i = 0;
+		for (Entry<String,Integer> foo : map.entrySet()) {
+			arr[i] = foo;
+			i++;
+		}
+
+		MergeSort.mergeSort(arr, new EntryComparator());
+		for (int j = 0; i < 10; i++)
+			System.out.println(arr[i]);
 	}
 }
 
